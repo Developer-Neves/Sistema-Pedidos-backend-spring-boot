62eef3586eecf7ead39c8c568f14cd2a3aa06be3
developerneves@gmail.com <developerneves@gmail.com>
2020-01-17 23:44:35 -0300

    Validação sintatica com Bean validation

--- src/main/java/com/jdnevesti/cursomc/services/CategoriaService.java
+++ src/main/java/com/jdnevesti/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jdnevesti.cursomc.domain.Categoria;
+import com.jdnevesti.cursomc.dto.CategoriaDTO;
 import com.jdnevesti.cursomc.repositories.CategoriaRepository;
 import com.jdnevesti.cursomc.services.exceptions.ObjectNotFoundException;
 import com.jdnevesti.cursomc.services.exceptions.DataIntegrityException;
@@ -61,5 +62,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page,Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
