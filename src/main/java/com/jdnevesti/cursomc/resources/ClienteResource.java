5ffa2686fbf5cde2b1794e8792182c27eef23e0d
developerneves@gmail.com <developerneves@gmail.com>
2020-01-20 11:25:20 -0300

    Inserindo um novo Cliente e Endereço com POST

--- src/main/java/com/jdnevesti/cursomc/resources/ClienteResource.java
+++ src/main/java/com/jdnevesti/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.jdnevesti.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jdnevesti.cursomc.domain.Cliente;
 import com.jdnevesti.cursomc.dto.ClienteDTO;
+import com.jdnevesti.cursomc.dto.ClienteNewDTO;
 import com.jdnevesti.cursomc.services.ClienteService;
 
 @RestController
@@ -40,7 +43,18 @@ public class ClienteResource {
 			List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
 			return ResponseEntity.ok().body(listDto);
 		}
-				
+		
+		// Método POST para inserir dados
+		@RequestMapping(method=RequestMethod.POST)
+		public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
+			Cliente obj = service.fromDTO(objDto);
+			obj = service.insert(obj);
+			//pega a URI do novo recurso que foi inserido
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{id}").buildAndExpand(obj.getId()).toUri();
+			return ResponseEntity.created(uri).build();
+		}
+		
 		// Método PUT para atualizar dados
 		@RequestMapping(value="/{id}", method=RequestMethod.PUT)
 		public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
