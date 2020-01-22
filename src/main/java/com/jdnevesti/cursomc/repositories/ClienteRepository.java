19e3aa51cf06a7b05fd164f4e7347150d646dae6
developerneves@gmail.com <developerneves@gmail.com>
2020-01-21 23:06:10 -0300

    Validação customizada: email não repetido na inserção de cliente

--- src/main/java/com/jdnevesti/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/jdnevesti/cursomc/repositories/ClienteRepository.java
@@ -2,9 +2,12 @@ package com.jdnevesti.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.jdnevesti.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
